8b756e68f101638b7263594810f0fcf8115b3155
DESKTOP-V49AJGE\PRECISISON M4700 <tomkoi236@gmail.com>
2017-02-28 14:53:40 +0700

    add entity framework code 1st for data

---
+++ OnlineShop.Data/Infrastructure/DBFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Data.Infrastructure
+{
+    public class DBFactory : Disposable, IDisposable
+    {
+        private OnlineShopDbContext dbContext;
+        public OnlineShopDbContext Init()
+        {
+            return dbContext ?? (dbContext = new OnlineShopDbContext());
+        }
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+            {
+                base.DisposeCore();
+            }
+        }
+
+    }
+}
