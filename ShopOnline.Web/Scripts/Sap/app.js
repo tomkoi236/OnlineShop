c323502c2c0ff6a78933b4d778db2530eed54e14
DESKTOP-V49AJGE\PRECISISON M4700 <tomkoi236@gmail.com>
2017-04-06 19:19:57 +0700

    create module

---
+++ ShopOnline.Web/Scripts/Sap/app.js
@@ -0,0 +1,10 @@
+﻿
+/// <reference path="../Plugins/angular/angular.js" />
+var myApp = angular.module('myModule', []);
+myApp.controller("myController", myController);
+
+myController.$inject = ['$scope'];
+//declera
+function myController($scope) {
+    $scope.message = "this is message from controller";
+}
