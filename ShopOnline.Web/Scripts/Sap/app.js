c9b7079dfe77468af9a3b1290d0417c8c6027520
DESKTOP-V49AJGE\PRECISISON M4700 <tomkoi236@gmail.com>
2017-04-11 09:37:13 +0700

    directive

--- ShopOnline.Web/Scripts/Sap/app.js
+++ ShopOnline.Web/Scripts/Sap/app.js
@@ -2,17 +2,36 @@
 /// <reference path="../Plugins/angular/angular.js" />
 var myApp = angular.module('myModule', []);
 myApp.controller("schoolController", schoolController);
-myApp.controller("studentController", studentController);
-myApp.controller("teacherController", teacherController);
-myController.$inject = ['$scope'];
+myApp.service('validator', validator);
+myApp.directive("onlineShopDirective", onlineShopDirective);
+
+schoolController.$inject = ['$scope', 'validator'];
 
 //declera
-function schoolController($scope) {
-    $scope.message = "anumation from school";
+function schoolController($scope, validator) {
+    $scope.checkNumber = function () {
+        $scope.message = validator.checkNumber(2);
+    }
 }
-function studentController( $scope) {
-//    $scope.message = "this is message from student";
+// declea service
+function validator($window) {
+    return {
+        checkNumber: checkNumber
+    };
+    function checkNumber(input) {
+        if (input % 2 === 0) {
+            return 'this is even';
+        }
+        else {
+            return 'this is odd';
+        }
+    }
 }
-function teacherController($scope) {
-    //$scope.message = "this is message from teacher";
+
+//directive
+function onlineShopDirective() {
+    return {
+        template: "<h1> this is my first custom directive </h1>"
+    }
 }
+
